ee760c9882bb6225d474aa9c22318acb7575501f
Tonysyj <sunyuejun8623@gmail.com>
2022-09-18 20:32:41 +1000

    01:32:22

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import fs from "node:fs"
 import path from "node:path"
+import {exec} from "node:child_process" // exec()是异步函数
 import { Command } from "commander"
 const program = new Command();
 // 设置
@@ -32,7 +33,28 @@ program.command('create <project> [other...]') // 此处是一个小bug，不加
   })
 
   function createProject(projectName){
-    console.log('mkdir:', projectName);
+    //   exec(`mkdir ${projectName}`, (err) => {
+    //     !err ? console.log(`mkdir ${projectName} success`) : console.log('ERROR:', err);
+    //   }) 此方法需要借助系统的力量，如果操作系统没有mkdir的命令则无法执行
+
+    /**fs.mkdirSync() 也是异步函数
+   * 作用跟exec一样.但是好处是不需要借助系统力量,一定有mkdir这个命令
+   * 不推荐在此步骤使用异步函数，因为需要保证前面的操作完成才能进行这步 
+   */
+  
+    fs.mkdir(path.join(projectName), (err) => {
+      !err ? console.log(`mkdir ${projectName} success`) : console.log('ERROR:', err);
+    }) // fs.mkdir是同步函数
+
+    fs.writeFile(path.join(projectName, '.gitignore'), `.vscode\n.DS_store\nnode_modules`, (err) => {
+      !err ? console.log(`create .gitignore success`) : console.log('ERROR:', err);
+    })
+
+    fs.writeFile(path.join(projectName, 'README.md'), `# ${projectName}`, (err) => {
+      !err ? console.log(`create readme success`) : console.log('ERROR:', err);
+    })  
+    exec(`cd ${projectName} \n git init \n npm init -y \n yarn add -D @types/node`)
+    exec(`code ${projectName}`)
   }
 
 // 解析
